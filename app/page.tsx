bc17e4c5965cbe4a35060d99a7b833c001615fda
kennzuki <kingsyken@gmail.com>
2024-07-02 17:01:57 +0300

    dynamic data for dasboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import DashBoardCard from '@/components/dasboard/DashboardCard';
-import {Button} from '@/components/ui/button'
+import { Button } from '@/components/ui/button'
+import { Folder, MessageCircle, Newspaper, User} from 'lucide-react';
 
 export default function Home() {
   return (
     <>
-      <div className="flex-col md:flex-row justify-between gap-5 mb-5">
-      <DashBoardCard/>
+      <div className="flex flex-col md:flex-row justify-between gap-5 mb-5">
+      <DashBoardCard title='Posts' count={100} icon={<Newspaper className='text-slate-500' size={72} />}/>
+      <DashBoardCard title='Categories' count={100} icon={<Folder className='text-slate-500' size={72} />}/>
+      <DashBoardCard title='Users' count={100} icon={<User className='text-slate-500' size={72} />}/>
+      <DashBoardCard title='Commentss' count={100} icon={<MessageCircle className='text-slate-500' size={72} />}/>
       </div>
     
    </>
--- components/dasboard/DashboardCard.tsx
+++ components/dasboard/DashboardCard.tsx
@@ -1,16 +1,20 @@
 import { Card, CardContent } from '@/components/ui/card';
-import { Newspaper} from 'lucide-react';
+import { LucideIcon} from 'lucide-react';
 
-
-const DashBoardCard = () => {
+interface DashboardCardProps{
+    title: string,
+    count: number,
+    icon:React.ReactElement<LucideIcon>
+}
+const DashBoardCard = ({title,count,icon}:DashboardCardProps) => {
     return <Card className='bg-slate-100 dark:bg-slate-800 p-4 pb-0'>
         <CardContent>
             <h3 className='text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200'>
-                Posts
+                {title}
             </h3>
             <div className="flex gap-5 justify-center items-center">
-                <Newspaper className='text-slate-500' size={72} />
-                <h3 className="text-5xl font-semibold text-slate-500 dark:text-slate-200">200</h3>
+                {icon}
+                <h3 className="text-5xl font-semibold text-slate-500 dark:text-slate-200">{count}</h3>
             </div>
       </CardContent>
   </Card>;
